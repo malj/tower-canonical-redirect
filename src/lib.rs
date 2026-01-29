4991fe763f76e471391508bc5438a81364023bd3
Luka Maljic <luka@maljic.com>
2026-01-29 14:43:24 +0100

    Expose public API

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
-#![allow(dead_code, unused_imports)]
-
 mod http;
 mod layer;
 mod service;
+
+pub use layer::CanonicalRedirectLayer;
+pub use layer::builder::CanonicalRedirectLayerBuildError;
+pub use layer::builder::CanonicalRedirectLayerBuilder;
+pub use service::CanonicalRedirect;
